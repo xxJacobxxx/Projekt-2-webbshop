8065728caf90ce6b78896f2fc489741e919aac26
Jacob <jacob.hansen@elev.ga.ntig.se>
2024-03-06 08:23:34 +0100

    Fixade början på localstorage och visar kundvagnen i datorform

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const main = document.querySelector("main")
 const aside = document.querySelector("aside")
 let shoppingcart = document.querySelector("ul")
+const visaSumma = document.querySelector("tot")
+const nyckel = document.querySelector("input")
 let totalSumma = 0;
+let jsonLista;
 let list = [];
 function add (namn, pris){
     let produkt = {
@@ -10,6 +13,7 @@ function add (namn, pris){
     }
     list.push(produkt)
     console.log(list)
+    listAdd();
 }
 function listAdd (){
     shoppingcart.innerHTML = ""
@@ -18,20 +22,32 @@ function listAdd (){
         listElement.innerText = list[i].namn + ": "+ list[i].pris+"kr",
         shoppingcart.appendChild(listElement);
         totalSumma = totalSumma + list[i].pris;
+        
     }
+    let jsonLista = JSON.stringify(list);
 }
+
 let oddOrEven = true
 function listDisplay(){
+    if(aside.style.display == "none"){
+        if(oddOrEven){
+            listAdd();
+            main.style.display = "none";
+            aside.style.display = "grid";
+            oddOrEven=false
+        }
+        else{
+            main.style.display = "flex";
+            aside.style.display = "none";
+            oddOrEven=true
+        }
+    }
+}
+function saveToLocalStorage (){
+    try {
+        window.localStorage.setItem(nyckel.value, jsonLista);
+    } catch (error) {
+        alert("Error in localstorage, try entering a username")
+    }
     
-    if(oddOrEven){
-        listAdd();
-        main.style.display = "none";
-        aside.style.display = "flex";
-        oddOrEven=false
-    }
-    else{
-        main.style.display = "block";
-        aside.style.display = "none";
-        oddOrEven=true
-    }
 }
